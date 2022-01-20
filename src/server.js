58a47dba6fef5126fd0b51c8b30e20b1e01c482f
Rishabh Agarwal <32418066+Rishabh130498@users.noreply.github.com>
2022-01-20 13:51:40 +0530

    transports added to server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const server = require('http').createServer();
 const os = require('os-utils');
 const port = '443' || '80';
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    transports: ['websocket', 'polling']
+  });
 
   io.on('connection', client => {
     setInterval(() => {
