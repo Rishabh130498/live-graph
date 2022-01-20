add05041e144e110cf02e574e5e67c117cf70ce6
Rishabh Agarwal <32418066+Rishabh130498@users.noreply.github.com>
2022-01-20 13:47:54 +0530

    remove transports

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const server = require('http').createServer();
 const os = require('os-utils');
 const port = '443' || '80';
-const io = require('socket.io')(server, {
-    transports: ['websocket', 'polling']
-  });
+const io = require('socket.io')(server);
 
   io.on('connection', client => {
     setInterval(() => {
